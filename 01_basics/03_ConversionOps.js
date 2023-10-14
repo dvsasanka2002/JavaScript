249fdbe2a1440adc246d066838c8e5fe2a8e7486
dvsasanka2002 <93524577+dvsasanka2002@users.noreply.github.com>
2023-10-14 07:49:59 +0000

    Conv of dtypes

---
+++ 01_basics/03_ConversionOps.js
@@ -0,0 +1,28 @@
+let score = "30abc"
+score = "0"
+score = undefined
+score = false
+score = NaN
+// console.log(typeof score);
+
+let val = Number(score)
+
+// console.log(typeof(val));
+// console.log(val); 
+// NaN for "30abc" , 
+//0 for null , 
+//NaN for undefined, 
+//1 for true, 
+//0 for false, 
+//NaN for NaN
+// console.log(typeof NaN); // give type as number
+
+let n = -25 // true for negative only 0 gives false
+let bool = Boolean(n)
+// console.log(typeof bool)
+// console.log(bool)
+let s = 1
+let v =  String(s)
+console.log(typeof v)
+console.log(v)
+
