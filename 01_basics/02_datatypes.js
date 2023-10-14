d6acdf29fa0e1a6d0fbd749f0c1d7f7352bae2e8
dvsasanka2002 <93524577+dvsasanka2002@users.noreply.github.com>
2023-10-14 07:37:53 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,24 @@
 // Not required all times by default run as strict
 
 // alert("hello") can be used in browser but not directly using nodeJS
+
+// Code Readability is Important
+// For Documentation use mdn documentation 
+// Official is tc39.es by ECMA Organization Standards
+
+let name = "Sasanka"
+let age = 8
+let isLog = false
+// number -> range upto pow(2,53)
+//bigint    
+//string => '' or ""
+// boolean = true or false
+// null => a standalone value 
+//undefined => no value assigned whereas null reperesents an empty value
+//symbol => unique identification
+
+//Object
+
+console.log(typeof age);
+console.log(typeof null); // null is a type of object
+console.log(typeof undefined); // undefined 
