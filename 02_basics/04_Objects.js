30420ed5b93ae7337b49353ade47bf2ceef79b7e
dvsasanka2002 <93524577+dvsasanka2002@users.noreply.github.com>
2023-10-14 15:49:53 +0000

    Objects

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -13,4 +13,38 @@ const regUser = {
 
     }
 }
-console.log(regUser.fullname.userfullname.firstname);
+// console.log(regUser.fullname?.userfullname.firstname); 
+// Optional Chaining ? : Checks if the path exists otherwise no response rather than error
+const obj1 = {
+    1 : "a",
+    2 : "b"
+}
+const obj2 = {
+    3 : "a",
+    4 : "b"
+}
+// const obj3 = {obj1,obj2}\
+// const obj3 = Object.assign({},obj1,obj2) // target,source Here {} is the source so that now we can have all others to be source
+// Can give any no of sources
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id : 1,
+        email : "sasanka"
+    },
+    {
+
+    },
+    {
+
+    }
+]
+// console.log(users[0].email)
+
+console.log(Object.keys(tinder));
+console.log(Object.values(tinder));
+console.log(Object.entries(tinder));
+
+console.log(tinder.hasOwnProperty("id"))
