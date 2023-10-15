c60865ed0c887affe1b513d6ed2b7ad7b6ddc5c5
dvsasanka2002 <93524577+dvsasanka2002@users.noreply.github.com>
2023-10-15 06:52:24 +0000

    Loops

---
+++ 05_Iterations/01_loops.js
@@ -0,0 +1,21 @@
+// for loop
+// use ctrl+D to perform refactoring
+// for (let i = 0; i < 11; i++) {
+//    console.log(i);
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         console.log(j)
+        
+//     }
+// }
+// array = [1,2,"Hi"]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+
+// break, continue
---
+++ 05_Iterations/02_while.js
@@ -0,0 +1,14 @@
+// while
+// let idx = 10
+// while (idx <= 20) {
+//     console.log(idx);
+//     idx++
+// }
+// let arr = [1,2,3]
+// let i = 0
+// while(i < arr.length)
+// {
+//     console.log(arr[i]);
+//     i++
+// }
+
