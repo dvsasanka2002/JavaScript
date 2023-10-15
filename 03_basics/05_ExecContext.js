26300f02936e994f0478177d31a8494505c69b75
dvsasanka2002 <93524577+dvsasanka2002@users.noreply.github.com>
2023-10-15 05:38:01 +0000

    ExecContext

---
+++ 03_basics/05_ExecContext.js
@@ -0,0 +1,11 @@
+// JS is Single Threaded Language
+// Global Exec Context in Browser is Window
+
+// JavaScript Execution Context
+// Global Exec Context is created first
+//Next Function Exec Context
+//Then Eval Exec Context
+
+//Two Phases
+// Memory Creation Phase
+// Execution Phase
